ff87cd290c156b29a49823b5974cd33e26db465f
davidcrows <dvdcrosson@gmail.com>
2020-09-28 09:59:31 +0200

    Board

--- Adventure/Program.cs
+++ Adventure/Program.cs
@@ -6,12 +6,84 @@ namespace Adventure
     {
         static void Main(string[] args)
         {
-            //Ser ni det här?
-            Console.WriteLine("David är här!");
-            Console.WriteLine("Elias är här!");
-            Console.WriteLine("Joseph är här!");
-            Console.WriteLine("Dylan är här!");
-            Console.WriteLine("Dylan är här!");
+
+            Board board = new Board(60, 30);
+
+            Console.ReadKey();
         }
     }
+
+    public class Board
+    {
+        public Board(Int32 x, Int32 y)
+        {
+            Int32[,] area = new Int32[y, x];
+
+            for (int i = 0; i < area.GetLength(0); i++)
+            {
+                for (int j = 0; j < area.GetLength(1); j++)
+                {
+                    if (i == 0 || j == 0 || i == area.GetLength(0) - 1 || j == area.GetLength(1) - 1)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+
+    public class Border
+    {
+        public static void Execute()
+        {
+            int topleft = 218;
+            int hline = 196;
+            int topright = 191;
+            int vline = 179;
+            int bottomleft = 192;
+            int bottomright = 217;
+
+            Console.OutputEncoding = System.Text.Encoding.GetEncoding(28591);
+            //draw top left corner
+            Write(topleft);
+            //draw top horizontal line
+            for (int i = 0; i < 10; i++)
+                Write(hline);
+            //draw top right corner
+            Write(topright);
+            Console.WriteLine();
+            //draw left and right vertical lines
+            for (int i = 0; i < 6; i++)
+            {
+                Write(vline);
+                for (int k = 0; k < 10; k++)
+                {
+                    Console.Write(" ");
+                }
+                WriteLine(vline);
+            }
+            //draw bottom left coner
+            Write(bottomleft);
+            //draw bottom horizontal line
+            for (int i = 0; i < 10; i++)
+                Write(hline);
+            //draw bottom right coner
+            Write(bottomright);
+            Console.ReadKey();
+        }
+        static void Write(int charcode)
+        {
+            Console.Write((char)charcode);
+        }
+        static void WriteLine(int charcode)
+        {
+            Console.WriteLine((char)charcode);
+        }
+    }
+
 }
